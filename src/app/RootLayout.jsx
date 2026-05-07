309a252463d5cfc1796605b320282e858efe3add
Simeon Rediger <simeonrediger@gmail.com>
2026-05-07 08:07:30 -0400

    Style header layout

--- src/app/RootLayout.jsx
+++ src/app/RootLayout.jsx
@@ -1,9 +1,10 @@
 import Navbar from '../components/Navbar/Navbar.jsx';
+import styles from './RootLayout.module.css';
 
 export default function RootLayout() {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <h1>Shop</h1>
         <Navbar />
       </header>
