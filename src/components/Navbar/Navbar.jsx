0f4b3b39ec4cea6751a2919ef6a413c93c730dcc
Simeon Rediger <simeonrediger@gmail.com>
2026-05-09 23:15:22 -0400

    Style active nav link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
 import { NavLink } from 'react-router';
 import styles from './Navbar.module.css';
 
+function getNavLinkClass({ isActive }) {
+  return isActive ? `${styles.link} ${styles.active}` : styles.link;
+}
+
 export default function Navbar() {
   return (
     <nav>
       <ul className={styles.list}>
         <li className={styles.item}>
-          <NavLink className={styles.link} to="/">
+          <NavLink className={getNavLinkClass} to="/">
             Home
           </NavLink>
         </li>
         <li className={styles.item}>
-          <NavLink className={styles.link} to="shop">
+          <NavLink className={getNavLinkClass} to="shop">
             Shop
           </NavLink>
         </li>
         <li className={styles.item}>
-          <NavLink className={styles.link} to="cart">
+          <NavLink className={getNavLinkClass} to="cart">
             Cart
           </NavLink>
         </li>
