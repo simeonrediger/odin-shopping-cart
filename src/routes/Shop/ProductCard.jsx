7c6c107dfe2601e1cb96bfec3626786f3117934d
Simeon Rediger <simeonrediger@gmail.com>
2026-05-11 23:41:14 -0400

    Add stars string to reflect product rating

--- src/routes/Shop/ProductCard.jsx
+++ src/routes/Shop/ProductCard.jsx
@@ -1,14 +1,22 @@
 import styles from './ProductCard.module.css';
 
 export default function ProductCard({ title, price, rating, image }) {
+  const stars = getStarsString(rating.rate);
+
   return (
     <article className={styles.card}>
       <img src={image} alt={title} />
       <h2 className={styles.name}>{title}</h2>
       <p>${price}</p>
       <p>
-        {rating.rate} ({rating.count})
+        {rating.rate} {stars} ({rating.count})
       </p>
     </article>
   );
+}
+
+function getStarsString(starAmount) {
+  const roundedStarAmount = Math.round(starAmount);
+  const hollowStarAmount = 5 - roundedStarAmount;
+  return '★'.repeat(roundedStarAmount) + '☆'.repeat(hollowStarAmount);
 }
