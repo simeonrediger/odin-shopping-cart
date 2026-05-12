eded4c71765d37b428d3683b07a5e258b490f935
Simeon Rediger <simeonrediger@gmail.com>
2026-05-11 23:51:45 -0400

    Add zero-padding to product price

--- src/routes/Shop/ProductCard.jsx
+++ src/routes/Shop/ProductCard.jsx
@@ -1,18 +1,24 @@
 import styles from './ProductCard.module.css';
 
 export default function ProductCard({ title, price, rating, image }) {
+  const formattedPrice = formatPrice(price);
   const stars = getStarsString(rating.rate);
 
   return (
     <article className={styles.card}>
       <img src={image} alt={title} />
       <h2 className={styles.name}>{title}</h2>
-      <p>${price}</p>
+      <p>{formattedPrice}</p>
       <p>
         {rating.rate} {stars} ({rating.count})
       </p>
     </article>
   );
+}
+
+function formatPrice(price) {
+  const [integerPart = '0', decimalPart = '0'] = String(price).split('.');
+  return `$${integerPart}.${decimalPart?.padEnd(2, 0)}`;
 }
 
 function getStarsString(starAmount) {
