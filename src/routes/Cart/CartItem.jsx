c882c28943ec958bae15d2df29bd1b1e3bc89f84
Simeon Rediger <simeonrediger@gmail.com>
2026-05-15 12:53:25 -0400

    Change cart item link color and text decoration

--- src/routes/Cart/CartItem.jsx
+++ src/routes/Cart/CartItem.jsx
@@ -6,8 +6,10 @@ export default function CartItem({ id, title, price, image, quantity }) {
   return (
     <div className={styles.cartItem}>
       <img className={styles.image} src={image} alt="" />
-      <Link to={`../shop/${String(id)}`}>
-        <h2 title={title}>{title}</h2>
+      <Link to={`../shop/${String(id)}`} className={styles.link}>
+        <h2 className={styles.name} title={title}>
+          {title}
+        </h2>
       </Link>
       <p>{price}</p>
       <p>{quantity}</p>
