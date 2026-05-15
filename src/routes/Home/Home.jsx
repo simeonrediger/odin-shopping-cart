1d8769103470fbdbc478e3ec8ba22bac17900e02
Simeon Rediger <simeonrediger@gmail.com>
2026-05-15 05:57:05 -0400

    Add Home content

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,3 +1,18 @@
+import styles from './Home.module.css';
+
+const currentMonth = new Date().toLocaleString('default', { month: 'long' });
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <div className={styles.container}>
+      <section className={styles.sectionContainer}>
+        <h2>About</h2>
+        <p>We sell things, stuff, and more!</p>
+      </section>
+      <section className={styles.sectionContainer}>
+        <h2>{currentMonth} Deals</h2>
+        <p>Spend $200 and get free 2-day shipping!</p>
+      </section>
+    </div>
+  );
 }
