f28982378d289b2747918b988c2cb38edd444b95
Simeon Rediger <simeonrediger@gmail.com>
2026-05-15 10:55:23 -0400

    Rename .container to .cartItem

--- src/routes/Cart/CartItem.jsx
+++ src/routes/Cart/CartItem.jsx
@@ -4,7 +4,7 @@ import styles from './CartItem.module.css';
 
 export default function CartItem({ id, title, price, image, quantity }) {
   return (
-    <div className={styles.container}>
+    <div className={styles.cartItem}>
       <img className={styles.image} src={image} alt="" />
       <Link to={`../shop/${String(id)}`}>
         <h2 title={title}>{title}</h2>
