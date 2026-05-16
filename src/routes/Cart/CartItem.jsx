82f2cb523dbe691c1349349a28f2a5deffa3fcb2
Simeon Rediger <simeonrediger@gmail.com>
2026-05-16 12:53:41 -0400

    Format prices

--- src/routes/Cart/CartItem.jsx
+++ src/routes/Cart/CartItem.jsx
@@ -1,6 +1,7 @@
 import { Link, useOutletContext } from 'react-router';
 
 import styles from './CartItem.module.css';
+import Price from '/src/components/Price.jsx';
 import QuantityInput from '/src/components/QuantityInput/QuantityInput.jsx';
 
 export default function CartItem({ id, title, price, image }) {
@@ -17,7 +18,7 @@ export default function CartItem({ id, title, price, image }) {
             {title}
           </h2>
         </Link>
-        <p>{price}</p>
+        <Price price={price} />
         <QuantityInput
           quantity={quantity}
           min={getMinItemQuantity()}
@@ -25,7 +26,7 @@ export default function CartItem({ id, title, price, image }) {
           onChange={quantity => onEditCart(id, quantity)}
         />
       </div>
-      <p>{price * quantity}</p>
+      <Price price={price * quantity} />
     </div>
   );
 }
