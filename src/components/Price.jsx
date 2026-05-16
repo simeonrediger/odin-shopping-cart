826bded5701f52940f6c8bfbd104a4179dd2fd75
Simeon Rediger <simeonrediger@gmail.com>
2026-05-16 14:59:52 -0400

    Add thousand separators

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -3,6 +3,11 @@ export default function Price({ className, price }) {
 }
 
 function formatPrice(price) {
-  const [integerPart = '0', decimalPart = '0'] = String(price).split('.');
+  let [integerPart = '0', decimalPart = '0'] = String(price).split('.');
+
+  for (let i = integerPart.length - 3; i > 0; i -= 3) {
+    integerPart = integerPart.slice(0, i) + ',' + integerPart.slice(i);
+  }
+
   return `$${integerPart}.${decimalPart?.padEnd(2, 0)}`;
 }
