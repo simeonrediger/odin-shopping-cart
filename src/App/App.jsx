b89d3e1a9cc0e25ef8aacbd9d31245a4baf26b7d
Simeon Rediger <simeonrediger@gmail.com>
2026-05-17 14:10:29 -0400

    Add cart summary

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -73,6 +73,7 @@ export default function App() {
       <main>
         <Outlet
           context={{
+            cart,
             getMaxItemQuantity,
             getCurrentItemQuantity,
             cartHasItem,
--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -3,10 +3,11 @@ import { useOutletContext } from 'react-router';
 
 import styles from './Cart.module.css';
 import CartItemRow from './CartItemRow.jsx';
+import Price from '/src/components/Price.jsx';
 
 export default function Cart() {
   const [products, setProducts] = useState([]);
-  const { cartHasItem } = useOutletContext();
+  const { cart, cartHasItem } = useOutletContext();
 
   useEffect(() => {
     function loadData() {
@@ -41,6 +42,28 @@ export default function Cart() {
             <CartItemRow key={product.id} {...product} />
           ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <th scope="row">Total</th>
+          <td>{getCartItemTotal(cart)} items</td>
+          <td>
+            <Price price={getCartPriceTotal(cart, products)} />
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
+}
+
+function getCartItemTotal(cart) {
+  return cart
+    .values()
+    .reduce((totalQuantity, quantity) => totalQuantity + quantity, 0);
+}
+
+function getCartPriceTotal(cart, products) {
+  return cart.entries().reduce((totalPrice, [productId, quantity]) => {
+    const product = products.find(product => product.id === productId);
+    return totalPrice + quantity * (product?.price ?? 0);
+  }, 0);
 }
