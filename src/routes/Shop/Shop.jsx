9999686be70177b3e586e29f47ded549791f8f9c
Simeon Rediger <simeonrediger@gmail.com>
2026-05-19 08:06:03 -0400

    Add error state to Shop page

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from 'react';
 
 import styles from './Shop.module.css';
+import PageFetchError from '/src/components/PageFetchError/PageFetchError.jsx';
 import PageLoader from '/src/components/PageLoader/PageLoader.jsx';
 import ProductCard from './ProductCard.jsx';
 
 export default function Shop() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     function loadData() {
       fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
+        .then(response => {
+          if (!response.ok) {
+            let errorMessage = `HTTP ${response.status}`;
+
+            if (response.statusText) {
+              errorMessage += `: ${response.statusText}`;
+            }
+
+            throw new Error(errorMessage);
+          }
+
+          return response.json();
+        })
         .then(data => {
           setProducts(data);
+        })
+        .catch(error => {
+          console.error(error);
+          setError(error);
         })
         .finally(() => {
           setLoading(false);
@@ -25,6 +43,10 @@ export default function Shop() {
 
   if (loading) {
     return <PageLoader />;
+  }
+
+  if (error) {
+    return <PageFetchError error={error} />;
   }
 
   return (
