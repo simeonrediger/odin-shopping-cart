3c12f8800efd6c452a9d94921bc4931af9c7c1e4
Simeon Rediger <simeonrediger@gmail.com>
2026-05-22 08:07:07 -0400

    Remove redundant closing tag

--- src/components/PageLoader/Loader/Loader.jsx
+++ src/components/PageLoader/Loader/Loader.jsx
@@ -1,7 +1,5 @@
 import styles from './Loader.module.css';
 
 export default function Loader() {
-  return (
-    <span className={styles.loader} role="img" aria-label="Loading"></span>
-  );
+  return <span className={styles.loader} role="img" aria-label="Loading" />;
 }
