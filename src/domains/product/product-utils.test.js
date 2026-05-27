f8351a1f626aa0ef5443a88560597d282b697918
Simeon Rediger <simeonrediger@gmail.com>
2026-05-27 13:02:28 -0400

    Test getStarsString()

--- src/domains/product/product-utils.test.js
+++ src/domains/product/product-utils.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { formatRate } from './product-utils.js';
+import { formatRate, getStarsString } from './product-utils.js';
 
 describe('formatRate()', () => {
   it('returns number as a string', () => {
@@ -13,5 +13,29 @@ describe('formatRate()', () => {
 
   it("appends '0' to integer", () => {
     expect(formatRate(3)).toBe('3.0');
+  });
+});
+
+describe('getStarsString()', () => {
+  it('returns 5 hollow stars for 0 stars', () => {
+    expect(getStarsString(0)).toBe('☆☆☆☆☆');
+  });
+
+  it('returns 5 solid stars for 5 stars', () => {
+    expect(getStarsString(5)).toBe('★★★★★');
+  });
+
+  it('returns 3 solid stars and 2 hollow stars for 3 stars', () => {
+    expect(getStarsString(3)).toBe('★★★☆☆');
+  });
+
+  describe('rounds to nearest whole star', () => {
+    it('3.5 stars becomes 4 stars', () => {
+      expect(getStarsString(3.5)).toBe('★★★★☆');
+    });
+
+    it('3.4 stars becomes 3 stars', () => {
+      expect(getStarsString(3.4)).toBe('★★★☆☆');
+    });
   });
 });
