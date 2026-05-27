5b4583d0b340e6f6f2c702d21ebd0e9f082fc05e
Simeon Rediger <simeonrediger@gmail.com>
2026-05-27 05:43:06 -0400

    Test cart item count in 'Cart' link

--- src/App/Navbar/Navbar.test.jsx
+++ src/App/Navbar/Navbar.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 
 import renderAppAtPath from '/tests/test-utils/render-app-at-path';
@@ -35,6 +35,40 @@ describe("doesn't give the .active class", () => {
     renderAppAtPath(path);
 
     expect(screen.getByRole('link', { name })).not.toHaveClass(styles.active);
+  });
+});
+
+describe("Cart item count in 'Cart' link", () => {
+  it('is hidden when cart item count is 0', () => {
+    renderNavbar({ cartItemCount: 0 });
+
+    const cartLink = screen.getByRole('link', { name: 'Cart' });
+
+    expect(within(cartLink).queryByTitle('0 items')).not.toBeInTheDocument();
+  });
+
+  describe('is shown when cart item count is at least 1 and no more than 99', () => {
+    it.each([1, 99])('cart item count = %d', cartItemCount => {
+      renderNavbar({ cartItemCount });
+
+      const cartLink = screen.getByRole('link', { name: 'Cart' });
+
+      expect(
+        within(cartLink).getByTitle(new RegExp(`^${cartItemCount} items?$`)),
+      ).toHaveTextContent(cartItemCount);
+    });
+  });
+
+  describe("is '99+' when when cart item count is at least 100", () => {
+    it.each([100, 256])('cart item count = %d', cartItemCount => {
+      renderNavbar({ cartItemCount });
+
+      const cartLink = screen.getByRole('link', { name: 'Cart' });
+
+      expect(
+        within(cartLink).getByTitle(`${cartItemCount} items`),
+      ).toHaveTextContent('99+');
+    });
   });
 });
 
