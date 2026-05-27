96213686bec9589c45967bbaf687d34159ddd41a
Simeon Rediger <simeonrediger@gmail.com>
2026-05-27 13:29:34 -0400

    Test focusing of page title heading

--- src/App/NotFoundPage/NotFoundPage.test.jsx
+++ src/App/NotFoundPage/NotFoundPage.test.jsx
@@ -1,4 +1,5 @@
 import { it, expect } from 'vitest';
+import { screen } from '@testing-library/react';
 
 import renderWithAppContext from '/tests/test-utils/render-with-app-context.jsx';
 import { buildDocumentTitle } from '/src/utils.js';
@@ -11,4 +12,10 @@ it('sets document title', () => {
   renderWithAppContext('/wrong-path', NotFoundPage);
 
   expect(document.title).toMatch(buildDocumentTitle('Page Not Found'));
+});
+
+it('focuses page title heading', () => {
+  renderWithAppContext('/wrong-path', NotFoundPage);
+
+  expect(screen.getByRole('heading', { name: 'Page not found' })).toHaveFocus();
 });
