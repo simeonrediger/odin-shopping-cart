f50217959d43344ac6a607e4e6c0417bcec72c3d
Simeon Rediger <simeonrediger@gmail.com>
2026-05-27 07:07:00 -0400

    Test getCurrentItemQuantity()

--- src/App/app-utils.test.js
+++ src/App/app-utils.test.js
@@ -1,9 +1,33 @@
 import { describe, it, expect } from 'vitest';
 
-import { MAX_QUANTITY_PER_ITEM, getMaxItemQuantity } from './app-utils.js';
+import {
+  MAX_QUANTITY_PER_ITEM,
+  getMaxItemQuantity,
+  getCurrentItemQuantity,
+} from './app-utils.js';
 
 describe('getMaxItemQuantity()', () => {
   it('returns MAX_QUANTITY_PER_ITEM', () => {
     expect(getMaxItemQuantity()).toBe(MAX_QUANTITY_PER_ITEM);
+  });
+});
+
+describe('getCurrentItemQuantity()', () => {
+  it('returns the quantity of the specified item', () => {
+    const cart = new Map();
+    cart.set(1, 3);
+    cart.set(2, 2);
+    const productId = 1;
+
+    expect(getCurrentItemQuantity(cart, productId)).toBe(3);
+  });
+
+  it('returns 0 if cart does not have the item', () => {
+    const cart = new Map();
+    cart.set(1, 3);
+    cart.set(2, 2);
+    const productId = 3;
+
+    expect(getCurrentItemQuantity(cart, productId)).toBe(0);
   });
 });
