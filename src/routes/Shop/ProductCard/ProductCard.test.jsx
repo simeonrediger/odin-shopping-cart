c3535c586a4e2784aa9ff31d6f0a508e556eff80
Simeon Rediger <simeonrediger@gmail.com>
2026-05-28 08:56:35 -0400

    Test that product name is shown

---
+++ src/routes/Shop/ProductCard/ProductCard.test.jsx
@@ -0,0 +1,31 @@
+import { it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import ProductCard from './ProductCard.jsx';
+
+function createProduct() {
+  return {
+    id: 1,
+    title: 'Product Name',
+    price: 10.99,
+    rating: { rate: 4.1, count: 123 },
+    image: 'https://example.com/image.png',
+  };
+}
+
+const context = Object.freeze({
+  getMaxItemQuantity: vi.fn(),
+  getCurrentItemQuantity: vi.fn(),
+  regulateQuantityToAdd: vi.fn(),
+  onAddToCart: vi.fn(),
+});
+
+it('shows product name', () => {
+  const product = createProduct();
+
+  render(<ProductCard {...product} {...context} />);
+
+  expect(
+    screen.getByRole('heading', { name: product.title }),
+  ).toBeInTheDocument();
+});
