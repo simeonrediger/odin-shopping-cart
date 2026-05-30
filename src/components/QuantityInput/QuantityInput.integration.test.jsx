61e11d6c0abb6c923d3bdb73f3e0c803f9213788
Simeon Rediger <simeonrediger@gmail.com>
2026-05-30 08:23:12 -0400

    Test increment behavior at max quantity

--- src/components/QuantityInput/QuantityInput.integration.test.jsx
+++ src/components/QuantityInput/QuantityInput.integration.test.jsx
@@ -3,20 +3,21 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import renderApp from '/tests/test-utils/render-app.jsx';
+import { MAX_QUANTITY_PER_ITEM } from '/src/domains/cart/cart-constants.js';
+
+const product = vi.hoisted(() => ({
+  id: 1,
+  title: 'Product 1',
+  price: 10.99,
+  rating: { rate: 4.1, count: 256 },
+  image: 'https://example.com/image.png',
+}));
 
 vi.mock('/src/hooks/useProducts.js', () => ({
   default: vi.fn().mockReturnValue({
     loading: false,
     error: null,
-    products: [
-      {
-        id: 1,
-        title: 'Product 1',
-        price: 10.99,
-        rating: { rate: 4.1, count: 256 },
-        image: 'https://example.com/image.png',
-      },
-    ],
+    products: [product],
   }),
 }));
 
@@ -32,5 +33,20 @@ describe('Increment button', () => {
     await user.click(incrementButton);
 
     expect(numberInput).toHaveDisplayValue(initialQuantity + 1);
+  });
+
+  it("doesn't increase quantity beyond maximum", async () => {
+    const user = userEvent.setup();
+    const cart = new Map();
+    cart.set(product.id, MAX_QUANTITY_PER_ITEM);
+
+    renderApp(['/shop'], cart);
+    const numberInput = screen.getByRole('spinbutton', { name: 'Quantity' });
+    const initialQuantity = +numberInput.value;
+    const incrementButton = screen.getByRole('button', { name: '+' });
+
+    await user.click(incrementButton);
+
+    expect(numberInput).toHaveDisplayValue(initialQuantity);
   });
 });
