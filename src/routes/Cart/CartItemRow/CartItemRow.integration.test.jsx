996c82cc70c37d1c0d5efee087e7f4e63a86e120
Simeon Rediger <simeonrediger@gmail.com>
2026-05-30 10:40:36 -0400

    Test quantity if below-minimum value provided

--- src/routes/Cart/CartItemRow/CartItemRow.integration.test.jsx
+++ src/routes/Cart/CartItemRow/CartItemRow.integration.test.jsx
@@ -43,4 +43,20 @@ describe('Quantity input', () => {
 
     expect(numberInput).toHaveDisplayValue(1);
   });
+
+  it('has a value of 1 if an even lower value is provided', async () => {
+    const user = userEvent.setup();
+    const modKey = navigator.platform.includes('Mac') ? 'Meta' : 'Control';
+    const cart = new Map();
+    cart.set(product.id, 5);
+
+    renderApp(['/cart'], cart);
+    const numberInput = screen.getByRole('spinbutton', { name: 'Quantity' });
+
+    await user.click(numberInput);
+    await user.keyboard(`{${modKey}>}a{/${modKey}}`);
+    await user.paste('-3');
+
+    expect(numberInput).toHaveDisplayValue(1);
+  });
 });
