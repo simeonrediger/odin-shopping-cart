6f80101fc87db536919308700e41dd03e92f9ea8
Simeon Rediger <simeonrediger@gmail.com>
2026-05-30 11:26:42 -0400

    Test cart total item quantity

--- src/routes/Cart/CartItemRow/CartItemRow.integration.test.jsx
+++ src/routes/Cart/CartItemRow/CartItemRow.integration.test.jsx
@@ -129,3 +129,13 @@ it('Item subtotal is correct', () => {
 
   expect(screen.queryAllByText('$45.00')[0]).toBeInTheDocument();
 });
+
+it('Cart total item quantity is correct', () => {
+  const cart = new Map();
+  cart.set(products[0].id, 3);
+  cart.set(products[1].id, 2);
+
+  renderApp(['/cart'], cart);
+
+  expect(screen.getByText('5 items')).toBeInTheDocument();
+});
